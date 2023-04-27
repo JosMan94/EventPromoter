245a2faf8ea7223e1d798a81f16e2058a81f8fa3
marco121942 <marco.sotomayor@tecsup.edu.pe>
2023-04-27 14:25:39 -0500

    vite v4

--- vite.config.js
+++ vite.config.js
@@ -10,12 +10,24 @@ export default ({ mode }) => {
     };
     return defineConfig({
         server: {
-            // port: process.env.VITE_PORT,
+            port: process.env.VITE_PORT,
             proxy: {
                 "/api": {
                     changeOrigin: true,
                     target: process.env.VITE_PROXY_URI,
-
+                    secure: false,
+                    ws: true,
+                    configure: (proxy, _options) => {
+                        proxy.on('error', (err, _req, _res) => {
+                            console.log('proxy error', err);
+                        });
+                        proxy.on('proxyReq', (proxyReq, req, _res) => {
+                            console.log('Sending Request to the Target:', req.method, req.url);
+                        });
+                        proxy.on('proxyRes', (proxyRes, req, _res) => {
+                            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+                        });
+                    },
                 },
             },
         },
